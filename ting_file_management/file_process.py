0837f3568d47a470527a7c06df89e0f3821b092f
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-04-10 15:23:12 -0400

    feat(file_process.py)  Implementa a capacidade de transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que é capaz de armazenar dentro da Queue - REQ03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    
+    archive = txt_importer(path_file)  # Dados do arquivo
+    data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(archive),
+            "linhas_do_arquivo": (archive),
+        }
+
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
+    return data
 
 
 def remove(instance):
