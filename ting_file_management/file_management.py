602e939e1f7ff8db7715ba0b9846c971e1dc21cb
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-04-10 15:01:40 -0400

    feat(file_management.py) Implementa uma exceção, caso o txt não exista, retorna uma mensagem de erro e se a extensão for diferente de txt, retorna formato invalido - REQ02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith('txt'):
+        return sys.stderr.write('Formato inválido\n')
+
+    try:
+        with open(path_file, mode="r") as file:
+            data = file.read().split('\n')
+            return data
+
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
