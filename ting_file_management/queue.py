d72e993ba37e4b72386728b31b236ab04914efb0
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-04-10 14:38:27 -0400

    feat(queue.py) Implementa os métodos de len, inserção, remoção e busca de dados na classe Queue - REQ01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -2,17 +2,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 
 class Queue(AbstractQueue):
-    def __init__(self):
-        """Iniciando o projeto ting"""
-
-    def __len__(self):
+    def __init__(self):  # Construtor
         """Aqui irá sua implementação"""
+        self.list = list()
 
-    def enqueue(self, value):
+    def __len__(self):  # Retorna o tamanho da lista
         """Aqui irá sua implementação"""
+        return len(self.list)
 
-    def dequeue(self):
+    def enqueue(self, value):  # Adiciona dados
         """Aqui irá sua implementação"""
+        return self.list.append(value)
 
-    def search(self, index):
+    def dequeue(self):  # Remove dados
         """Aqui irá sua implementação"""
+        return self.list.pop(0)
+
+    def search(self, index):  # Busca dados
+        """Aqui irá sua implementação"""
+        if 0 > index or index >= len(self.list):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.list[index]
